39b2a94d2f9bf28e258e846b5f2aeb454813ee5e
deesus <d.reddy@yandex.com>
2020-08-26 05:22:05 -0400

    Feat: implement dropout.

--- nn_binary_classification.py
+++ nn_binary_classification.py
@@ -20,10 +20,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from nn_utils import plot_decision_boundary, load_2D_dataset
 
-train_X, train_Y, test_X, test_Y = load_2D_dataset()
 
-
-class nn_class():
+class NN_Model():
     __PRINT_COST_INTERVAL = 2000
     
     def __init__(self,
@@ -37,15 +35,15 @@ class nn_class():
                  initialization=None,
                  print_cost=False):
         np.random.seed(1)
-        
+
         self.X = X
         self.Y = Y
 
         self.num_iterations = num_iterations
         self.learning_rate = learning_rate
-        self.print_cost = print_cost # boolean - determines if costs per interval should be printed
-        self.regularization = str(regularization).upper() # type of regularization to be implemented
-        self.lambda_ = lambda_ # lambda constant used for L2 regularization equation
+        self.print_cost = print_cost                        # boolean - determines if costs per interval should be printed
+        self.regularization = str(regularization).upper()   # type of regularization to be implemented
+        self.lambda_ = lambda_                              # lambda constant used for L2 regularization equation
 
         self.b = {}
         self.W = {}
@@ -54,24 +52,36 @@ class nn_class():
         self.db = {}
         self.dW = {}
         self.dA = {}
-        self.costs = [] # the cost at a given interval/number of iterations, __PRINT_COST_INTERVAL
-        
-        self.layers = [X.shape[0]] + hidden_layers + [1] # array of layer shapes (nodes per layer), including input and output layers
-        self.L = len(self.layers)-1 # number of layers
-        self.m = X.shape[1] # number of training examples
-               
-        # initialize parameters:
+        self.costs = []  # the cost at a given interval/number of iterations, __PRINT_COST_INTERVAL
+
+        self.is_training = True
+        self.dropout_mask = {}
+        self.layers = [X.shape[0]] + hidden_layers + [1]    # array of layer shapes (nodes per layer), including input and output layers
+        self.L = len(self.layers)-1                         # number of layers
+        self.m = X.shape[1]                                 # number of training examples
+
+        # initialize parameters, W and b:
         for l in range(1, self.L+1):
-            self.W[l] = np.random.randn(self.layers[l], self.layers[l-1])
-            self.b[l] = np.zeros((self.layers[l], 1))
+            # check if layer is dict object and determine layer sizes:
+            if type(self.layers[l]) == dict and 'size' in self.layers[l]:
+                layer_size = self.layers[l]['size']
+            else:
+                layer_size = self.layers[l]
+            if type(self.layers[l-1]) == dict and 'size' in self.layers[l-1]:
+                previous_layer_size = self.layers[l-1]['size']
+            else:
+                previous_layer_size = self.layers[l-1]
+
+            self.W[l] = np.random.randn(layer_size, previous_layer_size)
+            self.b[l] = np.zeros((layer_size, 1))
             
             # scale weights:
             initialization = str(initialization).upper()
             
             if initialization == 'HE':
-                self.W[l] *= 2 / np.sqrt(self.layers[l-1])
+                self.W[l] *= 2 / np.sqrt(previous_layer_size)
             elif initialization == 'XAVIER':
-                self.W[l] *= 1 / np.sqrt(self.layers[l-1])
+                self.W[l] *= 1 / np.sqrt(previous_layer_size)
             else:
                 self.W[l] *= 0.01
 
@@ -93,7 +103,7 @@ class nn_class():
 
     def sigmoid_gradient(self, Z):
         return self.sigmoid(Z) * (1 - self.sigmoid(Z))
-    
+
     def forward_prop(self):
         for l in range(1, self.L+1):
             Z = np.dot(self.W[l], self.A[l-1]) + self.b[l]
@@ -103,7 +113,14 @@ class nn_class():
                 self.A[l] = self.sigmoid(Z)
             else:
                 self.A[l] = self.relu(Z)
-                
+
+            # if dropout is set for layer:
+            # need to scale the values of neurons that weren't shut down:
+            if self.is_training and (type(self.layers[l]) == dict) and ('keep_prob' in self.layers[l]):
+                self.dropout_mask[l] = np.random.random((self.A[l].shape[0], self.A[l].shape[1])) < self.layers[l]['keep_prob']
+                self.A[l] *= self.dropout_mask[l]  # apply mask
+                self.A[l] /= self.layers[l]['keep_prob']  # scale by keep_probability
+
         Y_hat = self.A[self.L]
         return Y_hat
 
@@ -112,7 +129,7 @@ class nn_class():
             if l == self.L:
                 dZ = self.A[l] - self.Y
                 
-                # alternatively, we can compute dZ for layer L the long way -- replace the previous line with:
+                # # alternatively, we can compute dZ for layer L the long way -- replace the previous line with:
                 # self.dA[l] = -(np.divide(self.Y, self.A[l]) - np.divide(1-self.Y, 1-self.A[l]))
                 # dZ = self.dA[l] * self.sigmoid_gradient(self.Z[l]) 
             else:
@@ -121,11 +138,19 @@ class nn_class():
             self.db[l] = 1.0/self.m * np.sum(dZ, axis=1, keepdims=True)
             self.dA[l-1] = np.dot(self.W[l].T, dZ)
             self.dW[l] = 1.0/self.m * np.dot(dZ, self.A[l-1].T)
-            
+
+            # if dropout is set for layer,
+            # apply dropout mask to the same neurons as in forward prop:
+            if self.is_training and (type(self.layers[l-1]) == dict) and ('keep_prob' in self.layers[l-1]):
+                self.dA[l-1] *= self.dropout_mask[l-1]
+                self.dA[l-1] /= self.layers[l-1]['keep_prob']
+
+            # if L2 regularization is set:
             if self.regularization == 'L2':
                  self.dW[l] += self.lambda_/self.m * self.W[l]
 
     def update_parameters(self):
+        """ Update parameters step during gradient descent. """
         for l in range(1, self.L+1):
             self.W[l] -= (self.learning_rate * self.dW[l])
             self.b[l] -= (self.learning_rate * self.db[l])
@@ -149,14 +174,13 @@ class nn_class():
         return cost
     
     def train(self):
-        """
-        Trains the model after initialization.
-        """
+        """ Trains the model after initialization. """
+        self.is_training = True
         self.costs = []
         
-        for i in range(1, self.num_iterations+1):
+        for i in range(self.num_iterations+1):
             Y_hat = self.forward_prop()
-            gradients = self.back_prop()
+            self.back_prop()
 
             if i % self.__PRINT_COST_INTERVAL == 0:
                 cost = self.compute_cost(Y_hat)
@@ -183,9 +207,8 @@ class nn_class():
         plt.show()
     
     def predict(self, X):
-        """
-        Predict (binary classify)
-        """
+        """ Predict (binary classify). """
+        self.is_training = False
         self.A[0] = X
         self.m = X.shape[1]
         
@@ -195,28 +218,35 @@ class nn_class():
         return predictions
     
     def print_training_accuracy(self, X, Y):
-        """
-        Prints the accuracy of train/test sets.
-        """
+        """ Prints the accuracy of train/test sets. """
         predictions = self.predict(X)
         
         accuracy = np.mean(np.int8(predictions == Y))
-        print('Accuracy: %s%%' % accuracy)
-        
+        print('Accuracy: %s%%' % (accuracy * 100))
 
+
+# + pycharm={"name": "#%%\n"}
+########## main: ##########
+train_X, train_Y, test_X, test_Y = load_2D_dataset()
 
 # +
-########## main: ##########
 # hyper parameters:
-train_hidden_layers = [20, 3]
+train_hidden_layers = [{'size': 20, 'keep_prob': 0.8}, {'size': 3, 'keep_prob': 0.75}]
 train_learning_rate = 0.3
-train_num_iterations = 80000
+train_num_iterations = 60000
 
-new_model = nn_class(train_X, train_Y, train_hidden_layers, train_learning_rate, train_num_iterations, regularization="L2", initialization='xavier', print_cost=False)
+new_model = NN_Model(train_X,
+                     train_Y,
+                     train_hidden_layers,
+                     train_learning_rate,
+                     train_num_iterations,
+                     initialization='xavier',
+                     print_cost=False)
 # -
 
 new_model.train()
 
+# +
 print('----- Training set: -----')
 new_model.print_training_accuracy(train_X, train_Y)
 print('----- Test set: -----')
@@ -224,9 +254,13 @@ new_model.print_training_accuracy(test_X, test_Y)
 
 new_model.plot_cost()
 
+# + pycharm={"name": "#%%\n"}
 # plot decision boundary after training:
 plt.title("Model Decision Boundaries")
 axes = plt.gca()
 axes.set_xlim([-0.75,0.40])
 axes.set_ylim([-0.75,0.65])
 plot_decision_boundary(lambda x: new_model.predict(x.T), train_X, np.squeeze(train_Y))
+
+# + pycharm={"name": "#%%\n"}
+
